4940d8322b8a29ce58cb4e1871c9ad487715cfbc
Anand Thakker <vestibule@anandthakker.net>
2016-07-06 08:15:51 -0400

    Update docs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 module.exports = nanoraf
 
 // Only call RAF when needed
-// fn -> fn
+// (fn, fn?) -> fn
 function nanoraf (render, raf) {
   assert.equal(typeof render, 'function', 'nanoraf: render should be a function')
   assert.ok(typeof raf === 'function' || typeof raf === 'undefined', 'nanoraf: raf should be a function or undefined')
